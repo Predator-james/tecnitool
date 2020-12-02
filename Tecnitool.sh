08aa610d2f9b4a13763ce3dfbc31df3d073ee44a
Técnicos en hacking Domina <73371736+Predator-james@users.noreply.github.com>
2020-12-01 22:18:20 -0600

    Create Tecnitool.sh

---
+++ Tecnitool.sh
@@ -0,0 +1,73 @@
+#!bin/bash
+clear
+echo -e '\e[1;34m█▀▀▀ █▀▀█ █▀▀█ █▀▀█ █▀▀█\e[0m'
+echo -e '\e[1;34m█▀▀▀ █▄▄▀ █▄▄▀ █░░█ █▄▄▀\e[0m'
+echo -e '\e[1;34m█▄▄▄ █░▀█ █░▀█ ▀▀▀▀ █░▀█\e[0m'
+echo -e '\e[1;34m|   TÉCNICOS EN HACKING     |\e[0m'
+echo -e '\e[1;34m|                           |\e[0m'
+echo -e '\e[1;34m|      aprende bash         |\e[0m'
+echo -e '\e[1;34m|                           |\e[0m'
+echo -e '\e[1;34m|_______By:JAMESJET_________|\e[0m'
+echo -e '\e[1;33mBienvenidos a mi Herramienta, con esta herramienta te voy a enseñar lo basico de programacion en bash y a crear herramienta basica\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo -e '\e[1;33mpara comenzar a crear nuestra herramienta vamos a necesitar un editor de codigo en este caso necesitaremos el editor de codigo vim para instalarlo vamos a poner el siguiente comando en nuestra terminal pkg install vim -y\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo -e '\e[1;33mdespues elegimos el nombre de nuestra herramienta ejemplo Tecnicos ponemos vim Tecnicos.sh se nos abrira una pantalla sin nada si intentas escribir algo no te dejara porque estas en modo visual para comenzar a programar ponemos la letra a en la parte de abajo tendra que decir --INSERT--\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo -e '\e[1;33mahora si a comenzar a programar :) primero ponemos #!bin/bash y damos enter despues ponemos en la segunda linea de código echo "Hello World" ahora vamos a ver como quedo nuestro script para eso vamos a salir guardando cambios con lo siguiente, en la parte de arriba de el teclado hay una tecla que dice ESC vamos a presionarla y despues ponemos dos puntos y una x asi debe de quedar en la parte de abajo :x si es asi solo damos enter y listo.\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo -e '\e[1;33mahora vamos a escribir un Hello World con color vamos a nuestro editor de nuevo con el comando vim + el nombre.sh en nuestro caso es vim Tecnicos.sh y ponemos la letra a despues borramos el Hello World anterior y escribimos lo siguiente\e[0m'
+echo ""echo -e '\e[1;34mHello World\e[0m '""
+echo -e '\e[1;33mesto hara que el texto se vea de color azul resaltado\e[0m'
+echo -e '\e[1;33mejemplo\e[0m'
+echo -e '\e[1;34mHello World\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo -e '\e[1;33mel comando sleep hace que el usuario espere el tiempo que le pongas ejemplo sleep 6 esto hara que el usuario espere 6 segundos y despues saldra la siguiente palabra que escribas\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo -e '\e[1;31maqui te dejo la lista de colores bash ;)\e[0m'
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+sleep 5
+echo "Negro = 0;30"
+echo "Gris oscuro = 1;30"
+echo "Azul = 0;34"
+echo "Azul resaltado = 1;34"
+echo "Verde = 0;32"
+echo "Verde resaltado = 1;32"
+echo "Cian = 0;36"
+echo "Cyan resaltado = 1;36"
+echo "Rojo = 0;31"
+echo "Rojo resaltado = 1;31"
+echo "Púrpura = 0;35"
+echo "Púrpura resaltado = 1;35"
+echo "Café = 0;33"
+echo "Amarillo = 1;33"
+echo "Gris = 0;37"
+echo "Blanco = 1;37"
+sleep 4
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;32m---------------------------------------------------------------------------\e[0m'
+echo -e '\e[1;33mGracias por usar mi herramienta regalame una estrellita y suscribete a mi canal de Youtube\e[0m'
+sleep 1
+echo -e '\e[1;33mhttps://www.youtube.com/channel/UC8n1Gy-YuEgmuZWkAL1ZuHQ\e[0m'
+sleep 2
+echo "TÉCNICOS EN HACKING"
